949b64e2aa6b06e318fd1e3139c24fd10b18ec49
Tomas Estrada <tomasenriquea@hotmial.com>
2020-01-21 15:47:58 +0100

    Inicio de proyecto API

---
+++ src/main/java/startApp/StartApp.java
@@ -0,0 +1,10 @@
+package startApp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApp {
+    public static void main(String[] args) {
+
+    }
+}
